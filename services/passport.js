9cf5fd80a8d26ea0b028455e3431115461d257a7
Erickson Teixeira <rodox.k@hotmail.com>
2019-01-06 01:29:37 -0200

    alt proxy gstrategy

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,9 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: googleClientID,
     clientSecret: googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
+
 }, (accessToken, refreshToken, profile, done) => {
 
     User.findOne({googleId: profile.id}).then((existingUser) => { 
