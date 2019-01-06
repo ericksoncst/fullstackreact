81dedeb1b70a124684c9351be5c51e094bd07827
Erickson Teixeira <rodox.k@hotmail.com>
2019-01-06 20:36:09 -0200

    Header handler login sys

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, BrowserRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const Header = () => <h2>Header</h2>
+
+import Header from './Header';
+import Landing from './Landing';
+
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
 
 
-const App = () => {
-  return (
-    <div>
-        <BrowserRouter>
-            <div>
-                <Route exact path="/" component={Landing} />
-                <Route path="/surveys" component={Dashboard} />
-                <Route path="/survey_new" component={SurveyNew} />
+class App extends Component {
+
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route path="/survey_new" component={SurveyNew} />
+                    </div>
+                </BrowserRouter>
             </div>
-        </BrowserRouter>
-    </div>
-  )
+        )
+
+    }
 }
 
-export default App;
+export default connect(null, actions)(App);
