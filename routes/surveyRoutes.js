7bbe5f8fba07e8067ce79214915ba5a672ae70a4
Erickson Teixeira <rodox.k@hotmail.com>
2019-01-08 00:57:41 -0200

    Send config ok and tested

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -8,7 +10,7 @@ module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
         const { title, subject, body, recipients} = req.body;
 
-        const newSurvey = new Survey({
+        const survey = new Survey({
             title, 
             subject, 
             body, 
@@ -17,7 +19,8 @@ module.exports = app => {
             dateSent: Date.now()
         });
 
-        
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+        mailer.send().catch(error => console.log(error));
 
     });
 }
